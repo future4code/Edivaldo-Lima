e5af3d202a092ac601216413946960a0b371752e
eddvaz <edivaldo.vaz@hotmail.com>
2021-04-15 21:51:43 -0300

    modificado agumas partes do js

--- quinzena4/planner/index.js
+++ quinzena4/planner/index.js
@@ -1,16 +1,6 @@
-// function adicionarTarefa (){
-//     let tarefa = document.getElementById ("tarefa").value
-//     let diasSemana = document.getElementById ("dias-semana").value
-//     document.getElementById("tarefa").value = ""
-//     document.getElementById(diasSemana).innerHTML += `<p> ${tarefa} </p>`
-// }
-
-const pokemons = [
-    {nome: 'bulbasaur', tipo: 'grama'}
-    {nome: 'charmander', tipo: 'fogo'}
-    {nome: 'squirtle', tipo: 'agua'}
-]
-
-for (const pkmn of pokemons) {
-    console.log(`${pkmn.nome} é de ${pkmn.tipo}`)
+function adicionarTarefa (){
+    let tarefa = document.getElementById ("tarefa").value
+    let diasSemana = document.getElementById ("dias-semana").value
+    document.getElementById("tarefa").value = ""
+    document.getElementById(diasSemana).innerHTML += `<p> ${tarefa} </p>`
 }
