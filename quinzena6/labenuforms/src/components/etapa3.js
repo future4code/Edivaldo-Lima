a736477d904d381a2a29055edab2bb1b6f8d02da
eddvaz <edivaldo.vaz@hotmail.com>
2021-05-11 23:46:10 -0300

    exercicio-labenuforms

---
+++ quinzena6/labenuforms/src/components/etapa3.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class Etapa3 extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Etapa 3 - Informações Gerais de Ensino</h1>
+                <p> 7. Por que você não terminou um curso de graduação?</p>
+                <input></input>
+
+                <p>8. Você fez algum curso complementar?</p>
+                <select>
+                    <option>Não fiz curso complementar</option>
+                    <option>Curso técnico</option>
+                    <option>Curso de inglês</option>
+                </select>
+            </div>
+        )
+    }
+}
+export default Etapa3
